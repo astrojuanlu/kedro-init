eb5272d41f4fa4f7fd27fa508ffdfa18df222f58
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-12-03 21:41:01 +0100

    Add optional rich CLI
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- src/kedro_init/cli.py
+++ src/kedro_init/cli.py
@@ -2,10 +2,31 @@ from pathlib import Path
 
 import click
 
-from .init import init
+try:
+    from rich.console import Console
+
+    rich_available = True
+except ImportError:
+    rich_available = False
+
+from .init import init, init_steps
 
 
 @click.command()
 @click.argument("project_root", type=click.Path(exists=False))
-def cli(project_root):
-    init(Path(project_root))
+def cli(project_root: str):
+    project_root_path = Path(project_root)
+    if rich_available:
+        console = Console()
+        with console.status(
+            f"Initialising Kedro project in {project_root_path.absolute()}"
+        ):
+            for step_message in init_steps(project_root_path):
+                console.log(step_message)
+            console.log(
+                "[green]:large_orange_diamond: Kedro project successfully initialised!"
+            )
+    else:
+        click.echo(f"Initialising Kedro project in {project_root_path.absolute()}...")
+        init(project_root_path)
+        click.echo("\U0001f536 Kedro project successfully initialised!")
--- src/kedro_init/init.py
+++ src/kedro_init/init.py
@@ -1,19 +1,25 @@
-import pathlib
+from pathlib import Path
 
 from .build_config import get_or_create_build_config, init_build_config
 from .config_dirs import get_or_create_config_dirs, init_config_dir
 from .modules import get_or_create_modules, init_module
 
 
-def init(project_root: pathlib.Path):
+def init_steps(project_root: Path):
+    yield "Looking for existing package directories"
+
     existing, build_config = get_or_create_build_config(project_root)
     if not existing:
         init_build_config(project_root, build_config=build_config)
+
+    yield "Initialising config directories"
 
     config_dirs = get_or_create_config_dirs(project_root)
     for existing, target_config_dir in config_dirs.values():
         if not existing:
             init_config_dir(project_root, target_config_dir=target_config_dir)
+
+    yield "Creating modules"
 
     modules = get_or_create_modules(project_root, build_config=build_config)
     for existing, target_module_path, module_contents_path in modules.values():
@@ -23,3 +29,8 @@ def init(project_root: pathlib.Path):
                 target_module_path=target_module_path,
                 module_contents_path=module_contents_path,
             )
+
+
+def init(project_root: Path):
+    for _ in init_steps(project_root):
+        pass
