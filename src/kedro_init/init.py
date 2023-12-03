8e94db3de1488aadfcba8750a290ec15fe16bfc0
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-12-03 21:12:16 +0100

    Init modules
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- src/kedro_init/init.py
+++ src/kedro_init/init.py
@@ -1,9 +1,19 @@
 import pathlib
 
 from .build_config import get_or_create_build_config, init_build_config
+from .modules import get_or_create_modules, init_module
 
 
 def init(project_root: pathlib.Path):
     existing, build_config = get_or_create_build_config(project_root)
     if not existing:
         init_build_config(project_root, build_config=build_config)
+
+    modules = get_or_create_modules(project_root, build_config=build_config)
+    for existing, target_module_path, module_contents_path in modules.values():
+        if not existing:
+            init_module(
+                project_root,
+                target_module_path=target_module_path,
+                module_contents_path=module_contents_path,
+            )
---
+++ src/kedro_init/modules.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+TEMPLATES_PATH = Path(__file__).parent / "templates"
+MODULE_TEMPLATES = {
+    "pipeline_registry.py": TEMPLATES_PATH / "pipeline_registry.py",
+    "settings.py": TEMPLATES_PATH / "settings.py",
+}
+
+
+def get_or_create_modules(
+    project_root: Path,
+    *,
+    build_config: dict[str, str],
+    module_templates: dict[str, str] = None,
+):
+    if not module_templates:
+        module_templates = MODULE_TEMPLATES
+
+    package_name = build_config["package_name"]
+    package_dir = next(project_root.glob(f"*/{package_name}"), None)
+
+    modules = {}
+    for module_name in module_templates:
+        target_module = package_dir / module_name
+        if target_module.exists():
+            modules[module_name] = True, target_module, target_module
+        else:
+            modules[module_name] = False, target_module, module_templates[module_name]
+
+    return modules
+
+
+def init_module(
+    project_root: Path, *, target_module_path: Path, module_contents_path: Path
+):
+    with target_module_path.open("w") as fh:
+        fh.write(module_contents_path.read_text())
